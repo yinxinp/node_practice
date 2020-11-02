95663a6d42f7ddc9f78ee4042266b0f93419276d
潘阿峰 <yinxinpz@163.com>
2020-11-02 09:48:02 +0800

    ex03

--- ex03/index.js
+++ ex03/index.js
@@ -1,18 +1,19 @@
 const fs = require('fs')
+//! 暗号：二分查找
 module.exports.parser = path => {
     const readStream = fs.createReadStream(path)
     let reqData = [];
     let size = 0;
     return new Promise(resolve => {
-         // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgdmgd9gdggdcg9kgdwgdmgd9gdggdDgqDgdUgdvgqRgqdgdcgdggdwgdggqdgqlgywgdcgdggdwgdggywgcRgckgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdHgcvgdggdwgdggqDgdYgd=gdegdkgqRgdcgdggdwgdggqkgc9
-gywgywgywgywgywgywgywgywgywgywgywgywgdegdngR9gd9gywgqLgcRgywgdcgdggdwgdggqDgdPgd9gdvgdRgdwgdkgc9
-gywgywgywgywgywgywgywgywgRkgqkgc9
-gywgywgywgywgywgywgywgywgdmgd9gdggdcg9kgdwgdmgd9gdggdDgqDgdUgdvgqRgqdgd9gdvgdcgqdgqlgywgddgd=gdvgdqgdwgdngdUgdvgywgqRgqkgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgdcgdggdwgdggywgcRgywgcPgd=gddgddgd9gdmgqDgdqgdUgdvgdqgdggdwgqRgdmgd9gdHgcvgdggdwgdggqlgywgdegdngR9gd9gqkgc9
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdegdUgdPgRggd9gqRgcwg9kg9cg9qgqDgdYgdggdmgdegd9gqRgdcgdggdwgdggqDgdwgdUg9kgdwgdmgdngdvgdRgqRgqkgqkgqk
-gywgywgywgywgywgywgywgywgRkgqkgc9
-         // ##END##
+        readStream.on('data', chunk => {
+            reqData.push(chunk);
+            size += chunk.length
+        })
+        readStream.on('end', () => {
+            resolve(JSON.parse(Buffer.concat(reqData, size).toString()))
+        })
+        readStream.on('error', err => {
+            throw err
+        })
     })
 }
